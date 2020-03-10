0b4ad3d2fb67c3a9811b7cf275ac28736e1d15e1
Bakare Sulaimon <ecomputer83@gmail.com>
2020-03-10 06:01:11 +0100

    UI completed

--- constants/prices.js
+++ constants/prices.js
@@ -7,14 +7,14 @@ const DailyPrices = [
     {Id:5, Product: "LPG", Price: 3000.00, Unit: "kg"}
 ]
 
-const Depots = [{Id: 1, Name: "Apapa"}, {Id: 2, Name: "Delta"}, {Id: 3, Name: "Abuja"}]
+const Depots = [{Id: 1, Name: "Lagos"}, {Id: 2, Name: "Warri"}]
 
-const Orders = [{OrderId: "56:34:54:65:43", Quantity: 50000, ProductId: 1, ProductName: "PMS", Depot: 1, DepotName: "Apapa", Status:"In Progress",
+const Orders = [{OrderId: "56:34:54:65:43", Quantity: 50000, ProductId: 1, ProductName: "PMS", Depot: 1, DepotName: "Lagos", Status:"In Progress",
  Programing: [{TruckNo: "KYF34534", Quantity: 10000, Destination: "24, Old Yaba road, Yaba, Lagos", Status: "Waybill"},
  {TruckNo: "WER23D3", Quantity: 10000, Destination: "24, Old Yaba road, Yaba, Lagos", Status: "Dispatch"},
  {TruckNo: "GDR5642", Quantity: 10000, Destination: "24, Old Yaba road, Yaba, Lagos", Status: "Ticket"},
  {TruckNo: "DSX5467", Quantity: 5000, Destination: "24, Old Yaba road, Yaba, Lagos", Status: "Programmed"}]},
- {OrderId: "56:34:54:65:44", Quantity: 50000, ProductId: 2, ProductName: "DPK", Depot: 2, DepotName: "Apapa", Status: "Closed",
+ {OrderId: "56:34:54:65:44", Quantity: 50000, ProductId: 2, ProductName: "DPK", Depot: 2, DepotName: "Lagos", Status: "Closed",
  Programing: []}]
 
 export default {DailyPrices, Depots, Orders}
